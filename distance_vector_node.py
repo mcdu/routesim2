afa7be36608f056e5e1aa6ad6c83e588d06f040b
mcdu <32721382+mcdu@users.noreply.github.com>
2019-04-18 22:25:51 -0500

    Add files via upload

--- distance_vector_node.py
+++ distance_vector_node.py
@@ -1,23 +1,110 @@
 from simulator.node import Node
-
+import math, json
 
 class Distance_Vector_Node(Node):
     def __init__(self, id):
         super().__init__(id)
+        self.logging.debug("new node %d" % self.id)        
+        self.previously_seen = dict()
+        self.seq_num = 0
+        self.neighbor_link_cost = dict()
+        self.routing_table = {
+                               id: {id: (0, set())}
+                             }
+        self.forwarding_table = dict()
 
-    # Return a string
     def __str__(self):
-        return "Rewrite this function to define your node dump printout"
+        return json.dumps({'id': self.id, 'routing_table': self.routing_table})
 
-    # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
-        # latency = -1 if delete a link
-        pass
+        if latency == -1: # link deleted
+            self.neighbors.remove(neighbor)
+            del self.neighbor_link_cost[neighbor]
+        else:
+            if neighbor not in self.neighbors:
+                self.neighbors.append(neighbor)
+                # for now i won't chcek if needs adding to new_nodes bc message should eventually arrive
+                #if neighbor not in self.routing_table[self.id]:
+                #    self.routing_table[self.id][neighbor] = (math.inf, set())
+            self.neighbor_link_cost[neighbor] = latency
 
-    # Fill in this function
+        if self.update_dv(True):
+            self.send_dv_to_neighbors()
+
     def process_incoming_routing_message(self, m):
-        pass
+        dv_message = json.loads(m)
+        sender = dv_message[0]
+        sender_dv = dv_message[1]
+        seq_num = dv_message[2]
+
+        #adjust
+        if sender in self.previously_seen:
+            if self.previously_seen[sender] >= seq_num:
+                return
+        self.previously_seen[sender] = seq_num
+
+        parsed_sender_dv = dict()
+        for str_node, val in sender_dv.items():
+            parsed_sender_dv[int(str_node)] = (val[0], set(val[1])) # may need to unstring tuple val
+
+        dv_updated = False
+        for node in parsed_sender_dv:
+            # if find out about new node in dest field of recveied dv, change our dv entry to node to inf
+            if node not in self.routing_table[self.id]:
+                self.routing_table[self.id][node] = (math.inf, set())
+                dv_updated = True
+        self.routing_table[sender] = parsed_sender_dv
+
+        if self.update_dv(dv_updated):
+            self.send_dv_to_neighbors()
 
     # Return a neighbor, -1 if no path to destination
     def get_next_hop(self, destination):
-        return -1
+        if destination in self.forwarding_table:
+            return self.forwarding_table[destination]
+        else:
+            return -1
+
+
+    # Helper functions
+
+    def make_dv_message(self):
+        temp_dv = dict()
+        for key, val in self.routing_table[self.id].items():
+            temp_dv[key] = (val[0], list(val[1]))
+        dv_message = (self.id, temp_dv, self.seq_num)
+        return json.dumps(dv_message)
+
+    def update_dv(self, dv_updated):
+        # loop through known nodes
+        for node, cost_and_path_to_node in self.routing_table[self.id].items():
+            if node != self.id:
+                cost_to_node = cost_and_path_to_node[0]
+                path_to_node = cost_and_path_to_node[1]
+                new_cost_to_node = math.inf
+                next_hop_to_node = -1
+                for neighbor in self.neighbors:
+                    if neighbor in self.routing_table:
+                        neighbor_dv = self.routing_table[neighbor]
+                        if node in neighbor_dv and self.id not in neighbor_dv[node][1]: #if second cond failed then we wont msg neighbors about new inf node
+                            cost_via_neighbor_path = self.neighbor_link_cost[neighbor] + neighbor_dv[node][0]
+                            if cost_via_neighbor_path < new_cost_to_node:
+                                new_cost_to_node = cost_via_neighbor_path
+                                next_hop_to_node = neighbor
+
+                # update dv if best path length to node has changed
+                if cost_to_node != new_cost_to_node:
+                    new_path_to_node = set()
+                    if next_hop_to_node != -1:
+                        new_path_to_node = self.routing_table[next_hop_to_node][node][1]
+                        new_path_to_node.add(next_hop_to_node)
+                    self.routing_table[self.id][node] = (new_cost_to_node, new_path_to_node)
+                    dv_updated = True
+                    self.forwarding_table[node] = next_hop_to_node # may or may not have also changed
+        # return bool indicating if dv was updated at all
+        return dv_updated
+
+    def send_dv_to_neighbors(self):
+        dv = self.make_dv_message()
+        self.seq_num += 1
+        self.send_to_neighbors(dv)
--- link_state_node.py
+++ link_state_node.py
@@ -1,23 +1,129 @@
 from simulator.node import Node
-
+import json, math
 
 class Link_State_Node(Node):
     def __init__(self, id):
         super().__init__(id)
+        self.link_states = dict()
+        self.forwarding_table = dict()
 
-    # Return a string
     def __str__(self):
-        return "Rewrite this function to define your node dump printout"
+        return json.dumps({'id': self.id, 'link_states': self.link_states})
 
-    # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
-        # latency = -1 if delete a link
-        pass
 
-    # Fill in this function
+        if neighbor not in self.neighbors:
+            self.neighbors.append(neighbor)
+        elif latency == -1 and neighbor in self.neighbors:
+            self.neighbors.remove(neighbor)
+
+        link = frozenset((self.id, neighbor))
+
+        # Update my link state
+        if link in self.link_states:
+            seq_num = self.link_states[link][1] + 1
+        else:
+            seq_num = 0
+        self.link_states[link] = (latency, seq_num)
+
+        # Flood updated link state
+        self.flood_ls(link)
+
+        # If neighbor is new, update them with all my other, prior link states
+        if seq_num == 0:
+            self.update_neighbor(neighbor)
+
+        # Run Dijkstra using new link states to update my forwarding_table
+        self.update_forwarding_table()
+
     def process_incoming_routing_message(self, m):
-        pass
+        link_tuple, latency, seq_num, sender = json.loads(m)
+        link = frozenset(link_tuple)
+
+        # If message is old, send back the newer link state, and do nothing else
+        #TODO optimize?
+        if link in self.link_states:
+            if seq_num <= self.link_states[link][1]:
+                if seq_num < self.link_states[link][1]:
+                    self.send_ls_to(link, sender)
+                return
+
+        # Update my link state
+        self.link_states[link] = (latency, seq_num)
+
+        # Flood updated link state
+        self.flood_ls(link)
+
+        # Run Dijkstra using new link statesto update my forwarding_table
+        self.update_forwarding_table()
 
     # Return a neighbor, -1 if no path to destination
     def get_next_hop(self, destination):
-        return -1
+        #self.update_forwarding_table(destination)
+        if destination in self.forwarding_table:
+            return self.forwarding_table[destination]
+        else:
+            return -1
+
+    # Helper functions
+
+    def update_forwarding_table(self):
+    #def update_forwarding_table(self, destination):
+        done_nodes = set([self.id])
+
+        all_nodes = set()
+        for link in self.link_states:
+            all_nodes.update(link)
+
+        dv = {}
+        pred = {}
+        for n in all_nodes:
+            if n in self.neighbors:
+                dv[n] = self.link_states[frozenset((self.id,n))][0]
+                pred[n] = self.id
+            else:
+                dv[n] = math.inf
+
+        while done_nodes != all_nodes:
+            nodes_to_do = all_nodes.difference(done_nodes)
+            next_cheapest_node = list(nodes_to_do)[0]
+            for ntd in nodes_to_do:
+                if dv[ntd] < dv[next_cheapest_node]:
+                    next_cheapest_node = ntd
+            done_nodes.add(next_cheapest_node)
+
+            for ncn_neighbor in nodes_to_do:
+                cl = frozenset((ncn_neighbor, next_cheapest_node))
+                if cl in self.link_states:
+                    maybe_better_cost = dv[next_cheapest_node] + self.link_states[cl][0]
+                    if maybe_better_cost < dv[ncn_neighbor]:
+                        dv[ncn_neighbor] = maybe_better_cost
+                        pred[ncn_neighbor] = next_cheapest_node
+
+        # trace back path from dest
+        for destination in all_nodes:
+            if destination != self.id and destination not in pred:
+                self.forwarding_table[destination] = -1
+            elif destination != self.id:
+                p = destination
+                while pred[p] != self.id:
+                    p = pred[p]
+                self.forwarding_table[destination] = p
+
+    def make_ls_message(self, link):
+        latency, seq_num = self.link_states[link]
+        return json.dumps((tuple(link), latency, seq_num, self.id))
+
+    def flood_ls(self, link):
+        ls = self.make_ls_message(link)
+        self.send_to_neighbors(ls)
+
+    def send_ls_to(self, link, neighbor):
+        ls = self.make_ls_message(link)
+        self.send_to_neighbor(neighbor, ls)
+
+    def update_neighbor(self, neighbor):
+        link_with_neighbor = frozenset((self.id, neighbor))
+        for link in self.link_states:
+            if link != link_with_neighbor:
+                self.send_ls_to(link, neighbor)
